5c55d3aba190c7056539b319db49e83e48251135
adrirajcoder <113422485+adrirajcoder@users.noreply.github.com>
2024-11-21 10:11:18 +0000

    Learning variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144554
+
+//var and let both have the same function.
+//let is the updated version of let.
+//var is scope-blind but let is not.
+let accountEmail = "gdts@gmail.com"
+var accountPassword = "2345"
+accountCity = "Jaipur"
+
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
